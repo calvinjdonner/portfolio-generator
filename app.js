a949171c45d378ae95c9626e7ebaf35410faaf44
Calvin Donner <calvinjdonner@gmail.com>
2021-09-15 10:31:54 -0600

    set up inquirer prompt

--- app.js
+++ app.js
@@ -1,40 +1,27 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template');
+const inquirer = require('inquirer');
+console.log(inquirer);
 
-const profileDataArgs = process.argv.slice(2);
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
 
-console.log(profileDataArgs);
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-console.log(name, github);
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
 
-const pageHTML = generatePage(name, github);
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
-fs.writeFile('./index.html', pageHTML, err => {
-  if (err) throw err;
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     // This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//       console.log(profileDataArr[i]);
-//     }
-  
-//     console.log('================');
-  
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-//   };
-
-// printProfileData(profileDataArgs);
-
-// const generatePage = () => 'Name: Jane, Github: janehub';
-// console.log(generatePage());
 
 
   
